866a7a289e6d3ce58d469e934e6dba2102e0e623
shaan <shaanq420@gmail.com>
2025-04-04 21:17:12 +0530

    Exploring Http methods GET POST PUT DELETE

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 const express=require('express');
 const app=express();
 
-app.use('/sara',(req,res)=>{
-    res.send("Sara is my sister!");
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Shaan",lastname:"Qureshi",age:22,city:"Bhopal"});
 })
 
-app.use("/shaan",(req,res)=>{
-    res.send("Shaan is my brother!");
+app.post("/user",(req,res)=>{
+    res.send("Posted Successfully TO the DB");
+});
+
+app.put("/user",(req,res)=>{
+    res.send("Updated Successfully in the DB using PUT");
 })
 
-app.use("/",(req,res)=>{
+app.delete("/user",(req,res)=>{
+    res.send("Deleted Successfully from the DB using DELETE");
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("Updated Successfully in the DB using PATCH");
+})
+app.use("/test",(req,res)=>{
     res.send("Hello World!");
 })
 
