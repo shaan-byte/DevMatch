db3798365ff81cccf438bd0f0f88bb2d9d4ae1d5
shaan <shaanq420@gmail.com>
2025-04-04 21:56:20 +0530

    playing with params and req queries

--- src/app.js
+++ src/app.js
@@ -1,29 +1,10 @@
 const express=require('express');
 const app=express();
 
-app.get("/user",(req,res)=>{
+app.get("/user/:userid/:name/:password",(req,res)=>{
+    console.log(req.params);
     res.send({firstname:"Shaan",lastname:"Qureshi",age:22,city:"Bhopal"});
 })
-
-app.post("/user",(req,res)=>{
-    res.send("Posted Successfully TO the DB");
-});
-
-app.put("/user",(req,res)=>{
-    res.send("Updated Successfully in the DB using PUT");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Successfully from the DB using DELETE");
-})
-
-app.patch("/user",(req,res)=>{
-    res.send("Updated Successfully in the DB using PATCH");
-})
-app.use("/test",(req,res)=>{
-    res.send("Hello World!");
-})
-
 
 app.listen(7777,()=>{
     console.log("Server is running on port 7777");
