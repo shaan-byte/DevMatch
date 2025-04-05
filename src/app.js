0a91233651c4c57068c1fe7cb5b2750c2f7d5402
shaan <shaanq420@gmail.com>
2025-04-05 20:39:37 +0530

    playing with multiple route handlers and next

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 const express=require('express');
 const app=express();
-
-app.get("/user/:userid/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"Shaan",lastname:"Qureshi",age:22,city:"Bhopal"});
-})
+    //playing with multiple route handlers
+app.get("/user",[(req, res,next)=>{
+    console.log("Hello User 1!");
+    next();
+},(req,res,next)=>{
+    console.log("Hello User 2!");
+    next();
+},(req,res,next)=>{
+    console.log("Hello User 3!");
+    next();
+},(req,res,next)=>{
+    console.log("Hello User 4!");
+    //res.send("4th response!");
+    next();
+},(req,res,next)=>{
+    console.log("Hello User 5!");
+    res.send("Final Response!");
+}]);
 
 app.listen(7777,()=>{
     console.log("Server is running on port 7777");
