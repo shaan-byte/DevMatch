48e16d7a4b353268fb66bd5e3eb7beeda5d458ae
shaan <shaanq420@gmail.com>
2025-04-06 18:38:43 +0530

    find data from DB and feed API

--- src/app.js
+++ src/app.js
@@ -18,6 +18,34 @@ app.post("/Signup",async (req,res)=>{
     }
 })
 
+app.get("/User", async (req,res)=>{         //find data via email and GET
+    const email=req.body.emailID;           //get email from request body
+    try{
+        const Users=await User.find({emailID:email});   //find user by emailID
+        if(Users.length===0){
+            //if no user found with the given emailID
+            res.status(404).send("User not found")
+        }else{
+            res.send(Users)
+        }
+    }catch(err){
+        res.status(500).send("Error retrieving user",err.message)
+    }
+})
+
+//feed API for Users
+app.get("/feed",async (req,res)=>{
+    try{
+        const users=await User.find({});  //find all users
+        if(!users){
+            res.status(404).send("No users found")
+    }else{
+        res.send(users)
+    }
+}catch(err){
+    res.status(500).send("Error retrieving users"+err.message)
+}})
+
 connectDB().then(()=>{
     console.log("MongoDB connected successfully");
     app.listen(7777,()=>{
